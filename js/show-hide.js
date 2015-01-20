be2be2dc2ea4520c3cc829cbc60fdebf0f691e42
Igor Timohin <itimohin@NET-SIMPLE.RU>
2015-01-20 04:52:21 +0300

    journal page 90% done

--- js/show-hide.js
+++ js/show-hide.js
@@ -1,7 +1,10 @@
 function openbox(id){
 
 	elem = document.getElementById(id);
+	console.log(id);
+	
 	display = getComputedStyle(elem).display;
+	// data = id.getAttribute('data-toggle');
 
     if(display=='none'){
        document.getElementById(id).style.display='block';
